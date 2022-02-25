33be8d73045c9d764e3564d1e65e9114fb546fe4
David <Davidmaxsands@gmail.com>
2022-02-24 23:22:55 -0700

    commit tree a9e47695c9549592673953ab5285262d85fa3e88

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    sides_uniq = [@side1, @side2, @side3].uniq.size
+    sides = [@side1, @side2, @side3].sort
+    no_neg_size = sides.any?{|side| side <= 0} == false
+    triangle_inequality = sides[0] + sides[1] > sides[2] ? true : false
+
+   case 
+   when sides_uniq == 1 && no_neg_size && triangle_inequality
+    :equilateral
+   when sides_uniq == 2 && no_neg_size && triangle_inequality
+    :isosceles
+   when sides_uniq == 3 && no_neg_size && triangle_inequality
+    :scalene
+   else
+    raise TriangleError
+   end
+
+
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
